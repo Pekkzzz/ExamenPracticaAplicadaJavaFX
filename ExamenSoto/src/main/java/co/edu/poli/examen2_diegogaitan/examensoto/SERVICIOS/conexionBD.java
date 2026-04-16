fcc4b0350afccd9a92852a1d14f36daf530a15ad
Pekkzzz <diegogaitanvelandia1719@gmail.com>
2026-04-16 04:46:52 -0500

    ultimo

--- ExamenSoto/src/main/java/co/edu/poli/examen2_diegogaitan/examensoto/SERVICIOS/conexionBD.java
+++ ExamenSoto/src/main/java/co/edu/poli/examen2_diegogaitan/examensoto/SERVICIOS/conexionBD.java
@@ -7,30 +7,21 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class conexionBD {
 
+    private static final Dotenv dotenv = Dotenv.configure()
+            .directory(System.getProperty("user.dir"))
+            .ignoreIfMissing()
+            .load();
 
-
-    private static final Dotenv dotenv = Dotenv.load();
-
-    private static final String URL = dotenv.get("DB_URL");
-    private static final String USER = dotenv.get("DB_USER");
+    private static final String URL      = dotenv.get("DB_URL");
+    private static final String USER     = dotenv.get("DB_USER");
     private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     private static Connection connection;
 
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
-
-
-            // 🔍 PRUEBA TEMPORAL
-            System.out.println("URL: " + URL);
-            System.out.println("USER: " + USER);
-            System.out.println("PASSWORD: " + PASSWORD);
-
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
         }
         return connection;
-
-
     }
 }
-
--- ExamenSoto/src/main/java/module-info.java
+++ ExamenSoto/src/main/java/module-info.java
@@ -1,19 +1,20 @@
 module co.edu.poli.examen2_diegogaitan.examensoto {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.sql;
     requires org.junit.jupiter.api;
     requires mysql.connector.j;
     requires io.github.cdimascio.dotenv.java;
 
-// Abre los modelos para que JUnit y JavaFX puedan usar Reflexión
     opens co.edu.poli.examen2_diegogaitan.examensoto.MODELO;
-
-    // Abre controladores y vistas para JavaFX
     opens co.edu.poli.examen2_diegogaitan.examensoto.CONTROLADOR to javafx.fxml;
-    opens co.edu.poli.examen2_diegogaitan.examensoto.VISTA to javafx.fxml;
+
+    // ✅ Ahora javafx.graphics puede instanciar tu clase Application
+    opens co.edu.poli.examen2_diegogaitan.examensoto.VISTA to javafx.fxml, javafx.graphics;
+
     exports co.edu.poli.examen2_diegogaitan.examensoto;
     exports co.edu.poli.examen2_diegogaitan.examensoto.MODELO;
     exports co.edu.poli.examen2_diegogaitan.examensoto.CONTROLADOR;
-    exports co.edu.poli.examen2_diegogaitan.examensoto.VISTA;
+    exports co.edu.poli.examen2_diegogaitan.examensoto.SERVICIOS;
 }
